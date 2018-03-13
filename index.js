5daa7eebf712568de565ba663bbc1c11907a5873
Hanieh Shakeri <hshakeri@sfu.ca>
2018-03-13 12:55:32 -0700

    flickr images

--- index.js
+++ index.js
@@ -278,3 +278,23 @@ $.getJSON(str, function(data) {
  
   
 };
+    var keyword = "bunny";
+
+    $(document).ready(function(){
+
+        $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+        {
+            tags: keyword,
+            tagmode: "any",
+            format: "json"
+        },
+        function(data) {
+            var rnd = Math.floor(Math.random() * data.items.length);
+
+            var image_src = data.items[0]['media']['m'].replace("_m", "_b");
+
+            $('body').css('background-image', "url('" + image_src + "')");
+
+        });
+
+    });
