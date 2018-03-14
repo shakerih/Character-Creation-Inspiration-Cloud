af1b42cecdafc1089bd4c1943fae8d6112b74a58
Unknown <yla312@sfu.ca>
2018-03-14 01:34:42 -0700

    google images search

---
+++ imageSearch.js
@@ -0,0 +1,18 @@
+
+  (function() {
+    var cx = '016167563607305132578:yaakcr5sggs';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+
+
+// document.getElementById("gsc-i-id1").value;
+$('gsc-search-button-v2 ').attr('id', 'searchButton');
+// document.getElementById("abutton").onclick = function () {
+document.getElementsByClassName("gsc-search-button-v2").onclick = function () {
+    
+}
